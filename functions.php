17c192d63583ac917f282e5d56759d4867da59f6
EJ <ej@blackdogdigi.com>
2016-02-28 08:36:51 -0700

    Component Routing

--- functions.php
+++ functions.php
@@ -10,7 +10,6 @@ public static function componentRoute($componentName){
 
 	// The problem: get_template_part() only supports one directory level into the theme.
 
-	// TODO: Create function that calls the component with only the component name.
 	// TODO: Pass array of items that can be used in the component?
 
 	$componentRoute = 'components/'.$componentName.'/'.$componentName.'.php';
