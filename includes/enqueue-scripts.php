87fc9ad5f78911501389776ec19d97d0df9f6a0c
EJ <ej@blackdogdigi.com>
2016-06-02 15:36:03 -0600

    Added Bootstrap

--- includes/enqueue-scripts.php
+++ includes/enqueue-scripts.php
@@ -6,7 +6,19 @@ if ( ! function_exists( 'components_theme_enqueue' ) ) :
 	function components_theme_enqueue(){
 
 		// Enqueue the main Stylesheet.
+		wp_enqueue_style( 'styles', get_stylesheet_directory_uri() . '/assets/vendor/bootstrap/dist/css/bootstrap.css' );
 		wp_enqueue_style( 'styles', get_stylesheet_directory_uri() . '/assets/css/styles.css' );
+
+		// Enqueue Javascript Files
+		// jQuery
+		wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js', array(), '2.1.0', false );
+		
+		// Bootstrap	
+		wp_enqueue_script( 'bootstrap', get_stylesheet_directory_uri() . '/assets/vendor/bootstrap/dist/js/bootstrap.min.js', array('jquery'), '3.3.6', true );
+		
+		// Scripts
+		wp_enqueue_script( 'scripts', get_stylesheet_directory_uri() . '/assets/js/scripts.js', array('jquery', 'bootstrap'), '0.0.1', true );
+
 
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
