560d174956e9422e0f0715ade839d523f0de41dc
EJ <ej@blackdogdigi.com>
2017-07-03 11:25:40 -0600

    Revert

--- page.php
+++ page.php
@@ -7,21 +7,6 @@ get_header();
 
 	while ( have_posts() ) : the_post(); ?>
 	
-		<main class="container">
-		
-			<div class="row">
-				
-				<div class="col-xs-12">
-					
-					<h1><?php the_title(); ?></h1>
-					
-					<?php the_content(); ?>
-
-				</div>
-
-			</div>
-
-		</main>
 
 	<?php endwhile;
 
--- single.php
+++ single.php
@@ -7,14 +7,7 @@ get_header();
 	
 	while ( have_posts() ) : the_post();
 
-		renderComponent('post', get_post_format() );
-
-		the_post_navigation();
-
-		if ( comments_open() || get_comments_number() ) :
-			comments_template();
-		endif;
-
+		renderComponent('post', 'container' );
 
 	endwhile;
 	
