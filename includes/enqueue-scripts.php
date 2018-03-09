add51ef87258d3f602f5d8b1adbc8ed011958626
Lauren Zandstra <laurenezandstra@gmail.com>
2018-03-09 11:22:30 -0700

    changes

--- includes/enqueue-scripts.php
+++ includes/enqueue-scripts.php
@@ -17,6 +17,12 @@ if ( ! function_exists( 'components_theme_enqueue' ) ) :
 
 		// Bootstrap	
 		wp_enqueue_script( 'bootstrap', get_stylesheet_directory_uri() . '/assets/vendor/bootstrap-sass/assets/javascripts/bootstrap.min.js', array('jquery'), '3.3.6', true );
+
+		// Google Maps
+		// wp_enqueue_script( 'googlemaps', 'https://maps.googleapis.com/maps/api/js?key='.GOOGLE_API_KEY, array('jquery'), '2.8.2', true );
+
+		// Slick Carousel
+		// wp_enqueue_script( 'slick', get_stylesheet_directory_uri() . '/assets/vendor/slick-carousel/slick/slick.min.js', array('jquery'), '1.8.1', true );
 		
 		// Scripts
 		wp_enqueue_script( 'scripts', get_stylesheet_directory_uri() . '/assets/js/scripts.min.js?v='.CACHE_BUST, array('jquery', 'bootstrap'), '0.0.1', true );
