7b6566bd2b375805de31ee5b6660c6d4ee87a9ae
Jed Darrohn <jed@Jeds-MacBook-Pro.local>
2018-06-22 09:31:13 -0600

    Adding back in the header.js

---
+++ components/header/header.js
@@ -0,0 +1,39 @@
+;(function ($) {
+
+  $(document).ready(function(){
+
+      $('#mobile-menu').click(function(){
+          $(this).toggleClass('open');
+
+          $('.mobile-menu').fadeToggle();
+          $('.utility-menu').fadeToggle();
+      });
+
+  });
+
+
+  $(window).load(function(){
+
+
+  });
+
+
+  $(window).resize(function(){
+
+
+  });
+
+  /**
+   * Optional Pinned Header
+   */
+  $(window).scroll(function (event) {
+      var scroll = $(window).scrollTop();
+
+      if(scroll > 84){
+          $('header').addClass('scroll');
+      } else {
+          $('header').removeClass('scroll');
+      }
+  });
+
+})(jQuery);
