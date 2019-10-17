12e77f7e53214308089667a451b365c08e2909c2
Rebel <rebel@blackdogdigi.com>
2019-10-17 09:26:44 -0600

    404 page styled with an SVG graphic - bootstrap utilites added

--- 404.php
+++ 404.php
@@ -3,17 +3,19 @@
  * The 404 Template
  */
 
-get_header(); ?>
+ get_header(); ?>
 
-		<main class="container">
-			<div class="row">
-				<div class="col-xs-12">
+ 	<div id="primary" class="content-area text-center">
+ 		<div id="content" class="site-content" role="main">
+			<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1366 768" style="height: 550px; margin: -140px 0 -60px;
+"><g id="Layer_3" data-name="Layer 3"><polygon points="828.9 451.5 557.5 451.5 557.5 586.4 827.2 586.4 835.5 598.4 835.5 463.5 828.9 451.5" style="fill:red"/><path d="M827.1,419.5l6.4,15.9V620.3H574.1l-14.6-6.2V419.5H827.1m1.4-2h-271v198l16.2,6.8H835.5V435l-7-17.5Z"/></g><g id="Layer_2" data-name="Layer 2"><path d="M826.5,419.5,827,614l-267.5-.5v-194h267m2-2h-271v198L829,616l-.5-198.5Z"/><path d="M615.4,426.1a2.8,2.8,0,0,1,2.8,2.8,2.8,2.8,0,1,1-2.8-2.8m0-2a4.8,4.8,0,0,0-4.8,4.8,4.8,4.8,0,1,0,9.6,0,4.8,4.8,0,0,0-4.8-4.8Z"/><path d="M769.1,426.1a2.8,2.8,0,0,1,2.8,2.8,2.8,2.8,0,0,1-5.6,0,2.8,2.8,0,0,1,2.8-2.8m0-2a4.8,4.8,0,0,0-4.8,4.8,4.8,4.8,0,1,0,4.8-4.8Z"/><polyline points="769.1 428.9 692.8 292.6 615.4 428.9" style="fill:none;stroke:#000;stroke-miterlimit:10;stroke-width:2px"/><path d="M697.8,305.2a5,5,0,0,1-10,0v-15h10v15" style="fill:#fff;stroke:#000;stroke-miterlimit:10;stroke-width:2px"/><circle cx="692.8" cy="290.2" r="5" style="fill:#fff;stroke:#000;stroke-miterlimit:10;stroke-width:2px"/></g><g id="Layer_4" data-name="Layer 4"><text transform="translate(598.34 553.23)" style="isolation:isolate;font-size:94.90879821777344px;fill:#fff;font-family:Arial-Black, Arial Black">404</text></g></svg>
+			<div class="page-wrapper">
+				<h1 class="page-title">Not Found</h1>
+				<div class="page-content">
+					<h2 class="mb-5">Sorry, this page isn't available.</h2>
+				</div><!-- .page-content -->
+			</div><!-- .page-wrapper -->
+ 		</div><!-- #content -->
+ 	</div><!-- #primary -->
 
-					<h1>Looks like you're lost</h1>
-					<p><a href="/">Go Home >></a></p>
-					
-				</div>
-			</div>
-		</main>
-	
-<?php get_footer(); ?>
+ <?php get_footer(); ?>
