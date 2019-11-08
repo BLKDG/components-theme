29e615db311524bf6b5ad8a8341cd2bf36da879b
Rebel <rebel@blackdogdigi.com>
2019-11-08 11:45:43 -0700

    added copyright w/year - privacy and legal link added - blkdg link

--- components/footer/views/legal.php
+++ components/footer/views/legal.php
@@ -4,5 +4,10 @@
 			'theme_location' 	=> 'legal',
 			'menu_class'     	=> 'legal-menu'
 		 ) );
-	?>	
+	?>
+	<div class="legal-content text-center">
+		Copyright © <?php echo date("Y"); ?>  - All rights reserved.
+		<a target="_blank" class="link" href="/privacy-policy">Privacy & Legal</a>
+		Design & Build by <a target="_blank" class="link" href="https://www.blkdg.com">BLKDG</a>
+	</div>
 </div>
