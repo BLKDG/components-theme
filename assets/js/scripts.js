6dda2b19bd98e76ec00ff2057ea3e1deb477bf6c
Matt Greenwood <mattgreenwood@Matts-MacBook-Pro.local>
2021-09-22 09:52:56 -0600

    upadting

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,51 +1,10 @@
 $(window).load(function(){
 
-<<<<<<< HEAD
-	$(document).on('ready',function(){
-=======
     // Only animate elems above the fold. Everything else animate on scroll
     $('.anim-up:not(.anim-no-load), .anim-left:not(.anim-no-load), .anim-right:not(.anim-no-load)').addClass('anim-complete');
->>>>>>> e83737a0d973cf1afae464fc87d451065b784d36
     
 });
 
-<<<<<<< HEAD
-	});
-
-
-	$(window).load(function(){
-
-		// Only animate elems above the fold. Everything else animate on scroll
-        $('.anim-load').addClass('anim-complete');
-        
-	});
-
-    $(window).on('scroll',function() {
-
-        $('.anim-no-load').each(function(i, el) {
-            var el = $(el);
-            if (el.visible(true)) {
-            el.addClass('anim-complete'); 
-            }
-        });
-
-        $('.anim-up, .anim-left, .anim-right').each(function(i, el) {
-            var el = $(el);
-            if (el.visible(true)) {
-            el.addClass('anim-complete'); 
-            }
-        });
-
-    });
-
-
-	$(window).on('resize',function(){
-
-	});
-
-	$.fn.visible = function(partial) {
-
-=======
 $(window).on('scroll', function() {
 
     $('.anim-no-load').each(function(i, el) {
@@ -59,7 +18,6 @@ $(window).on('scroll', function() {
 
 $(window).on('resize', function(){
 });
->>>>>>> e83737a0d973cf1afae464fc87d451065b784d36
 
 $.fn.visible = function(partial) {
 
