ce9f2b4a530150f957c584e1556caf4ceefde1a4
EJ Uery <ej@blackdogdigi.com>
2022-04-05 15:22:20 -0600

    Auto-cache

--- setup/Scripts.php
+++ setup/Scripts.php
@@ -12,12 +12,11 @@ class Scripts
 	    add_action( 'wp_enqueue_scripts', array(__CLASS__, 'enqueue') );
     }
 
-    public static function enqueue()
-    {
+    public static function enqueue(){
         // Deregister WP jQuery
         if ( !is_admin() ) wp_deregister_script('jquery');
 
-        wp_enqueue_style( 'styles', get_stylesheet_directory_uri() . '/public/styles.css' );
+        wp_enqueue_style( 'styles', get_stylesheet_directory_uri() . '/public/styles.css?v='.filemtime(get_stylesheet_directory() . '/public/styles.css') );
 
         // Enqueue Javascript Files
         // jQuery
@@ -33,7 +32,7 @@ class Scripts
         wp_enqueue_script( 'slick', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js', array('jquery'), '1.8.1', true );
 
         // Scripts
-        wp_enqueue_script( 'scripts', get_stylesheet_directory_uri() . '/public/scripts.js', array(), '0.0.1', true );
+        wp_enqueue_script( 'scripts', get_stylesheet_directory_uri() . '/public/scripts.js?v='.filemtime(get_stylesheet_directory() . '/public/scripts.js'), array(), '0.0.1', true );
 
         if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
             wp_enqueue_script( 'comment-reply' );
